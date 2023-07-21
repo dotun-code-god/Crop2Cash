2514e66717f5a615e73a973afff7a303f5dbd1d3
dotun-code-god <dotun494@gmail.com>
2023-07-21 21:35:28 +0100

    changed insertion route to one

--- models/connect.js
+++ models/connect.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql');
 const util = require('util');
+require('dotenv').config();
 
 const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'crop2cash'
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
 });
 
 const query = util.promisify(con.query).bind(con);
