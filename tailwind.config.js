ec7b2c0677179e8fff3363a397eb7c77ffa342a7
dotun-code-god <dotun494@gmail.com>
2023-07-22 02:48:23 +0100

    added responsivenes to design

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
         c2c_primary: '#ffc508',
         c2c_grey: '#e7e7e7',
         add_farmer_input: '#c39b18',
+      },
+      screens: {
+        xs: {max: '450px'}
       }
     },
   },
